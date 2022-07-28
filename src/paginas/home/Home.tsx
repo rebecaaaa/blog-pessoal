ab0844df4ade72227de596e95a2e744d6879a56b
Rebeca <beca.azevedo00@gmail.com>
2022-07-27 23:04:14 -0300

    base do projeto

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+  return(
+    <>
+      <h1 className='titulo'>Home</h1>
+      <p>Teste</p>
+    </>
+  )
+}
+export default Home;
