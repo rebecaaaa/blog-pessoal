4a8b1d0acd0c7fb3308e318831b44da57d32b0b3
Rebeca <beca.azevedo00@gmail.com>
2022-07-28 13:52:15 -0300

    base do projeto

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './Home.css';
+import { countReset } from "console";
+import React from "react";
+import "./Home.css";
+import { Grid, Paper } from "@material-ui/core";
 
-function Home(){
-  return(
+function Home() {
+  return (
     <>
-      <h1 className='titulo'>Home</h1>
-      <p>Teste</p>
+      <h1 className="titulo">Home</h1>
     </>
-  )
+  );
 }
 export default Home;
