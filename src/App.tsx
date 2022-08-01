873591366de47921c641c197114d96c94320edce
Rebeca <beca.azevedo00@gmail.com>
2022-08-01 07:55:52 -0300

    add rotas e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 import { Grid } from "@material-ui/core";
 import Home from "./paginas/home/Home";
 import "./App.css";
+import Login from "./paginas/login/Login";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
+    <Router>
+    <Navbar />
+    <Routes>
+
+      <Route path="/" element={<Login />} />
+
+      <Route path="/home" element={<Home />} />
+
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
+    <Footer />
+  </Router>
   );
 }
 
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import {Box} from '@mui/material'
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
 function Navbar() {
     return (
       <>
@@ -28,11 +31,13 @@ function Navbar() {
                           temas
                       </Typography>
                   </Box>
+                  <Link to='/login' className='text-decorator-none'>
                   <Box mx={1} style={{ cursor: "pointer" }}>
                       <Typography variant="h6" color="inherit">
                           cadastrar tema
                       </Typography>
                   </Box>
+                  </Link>
                   <Box mx={1} style={{ cursor: "pointer" }}>
                       <Typography variant="h6" color="inherit">
                           logout
